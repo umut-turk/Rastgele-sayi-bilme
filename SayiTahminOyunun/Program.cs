e28ef0c4c134e20b2360e444f5978c8f4b2e214e
umut-turk <113532432+umut-turk@users.noreply.github.com>
2023-08-03 20:53:22 +0300

    Rastgele sayi bilme

---
+++ SayiTahminOyunun/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiTahminOyunun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("sayı  tahim  edin");
+
+            int tahmin_sayı=int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int rastgeleSayı = random.Next(1, 6);
+            if (tahmin_sayı==rastgeleSayı)
+            {
+                Console.WriteLine(rastgeleSayı + " " +" idi" +" " +"bildiniz");
+            }
+            else
+            {
+                Console.WriteLine(rastgeleSayı + " " + " idi" + " " + "bilemediniz");
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
